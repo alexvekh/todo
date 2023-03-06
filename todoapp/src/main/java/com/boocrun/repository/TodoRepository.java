0e83f21886f6da1129124608cff4600e5aa21f83
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-03-05 23:24:22 -0500

    conected front with backend

--- todoapp/src/main/java/com/boocrun/repository/TodoRepository.java
+++ todoapp/src/main/java/com/boocrun/repository/TodoRepository.java
@@ -9,15 +9,17 @@ import com.boocrun.domain.TodoItem;
 
 @Repository
 public class TodoRepository {
-    
+    private Integer idCounter = 0;
     private List<TodoItem> todoItems = new ArrayList<>();
     
     public List<TodoItem> fetchAllTodoItems (){
 	if (todoItems.size() == 0) {
 	    TodoItem item1 = new TodoItem();
+	    item1.setId(idCounter++);
 	    item1.setIsDone(false);
 	    item1.setTask("Task #1");
 	    
+	    todoItems.add(item1);
 	}
 	
 	return todoItems;	
