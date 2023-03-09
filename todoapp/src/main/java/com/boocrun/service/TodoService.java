010a32a3b34fbffc7611ef177a251e62cd62f608
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-03-08 23:20:31 -0500

    coded PutMapping. Works well

--- todoapp/src/main/java/com/boocrun/service/TodoService.java
+++ todoapp/src/main/java/com/boocrun/service/TodoService.java
@@ -1,6 +1,7 @@
 package com.boocrun.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,20 @@ public class TodoService {
     
     public List<TodoItem> fetchAllTodoItems(){
 	return todoRepo.fetchAllTodoItems();
+    }
+
+    public TodoItem updateTodoItem(Integer id, TodoItem todoItem) {
+	// update DB
+	Optional<TodoItem> todoOpt = todoRepo.fetchAllTodoItems()
+                                		.stream()
+                                		.filter(item -> item.getId().equals(id))
+                                		.findAny();
+	if (todoOpt.isPresent()) {
+	    TodoItem item = todoOpt.get();
+	    item.setIsDone(todoItem.getIsDone());
+	    item.setTask(todoItem.getTask());
+	    return item;
+	}
+	return null;
     }
 }
